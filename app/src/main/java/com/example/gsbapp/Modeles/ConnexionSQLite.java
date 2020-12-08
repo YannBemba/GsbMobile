1db351611693f0e7ba8c96ea078a5ceff868db45
FernandoGzz <71329068+FernandoGzz@users.noreply.github.com>
2020-12-08 11:18:28 -0600

    Add files via upload

--- app/src/main/java/com/example/gsbapp/Modeles/ConnexionSQLite.java
+++ app/src/main/java/com/example/gsbapp/Modeles/ConnexionSQLite.java
@@ -8,7 +8,44 @@ import androidx.annotation.Nullable;
 
 public class ConnexionSQLite extends SQLiteOpenHelper {
 
-    final String create_table_visiteur = "CREATE TABLE visiteur (nom text, prenom text, adresse text, cp int, login text, mdp text);";
+
+    public static final String DB_NAME = "DBgsb.db";
+    public static final int DB_VERSION = 1;
+
+    private final Context context;
+    private static ConnexionSQLite modele = null;
+
+
+
+
+
+
+
+    // constructeur - Singleton
+    public static ConnexionSQLite getInstance(Context context) {
+        if (modele == null) {
+            modele = new ConnexionSQLite(context);
+        }
+        return modele;
+    }
+
+    private ConnexionSQLite(Context context) {
+        // crée un objet pour créer, ouvrir et gérer une BDD Sqlite
+        super(context, DB_NAME, null, DB_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase DB_NAME) {
+        
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase DB_NAME, int i, int i1) {
+
+    }
+
+    /*final String create_table_visiteur = "CREATE TABLE visiteur (nom text, prenom text, adresse text, cp int, login text, mdp text);";
     final String create_table_1 = "CREATE TABLE CompteRendu (); ";
     final String create_table_3 = "CREATE TABLE  ();";
 
@@ -27,5 +64,5 @@ public class ConnexionSQLite extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase DBgsb, int i, int i1) {
         DBgsb.execSQL("DROP TABLE IF EXISTS visiteur");
         onCreate(DBgsb);
-    }
+    }*/
 }
